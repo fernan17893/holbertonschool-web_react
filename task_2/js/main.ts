3b8b51ef9ef48996a228b8ef4c04d9fa0c42b83a
fernan17893 <fernan17893@gmail.com>
2023-08-25 16:25:50 -0400

    task done

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -41,6 +41,33 @@ function createEmployee(salary: number | string): Director | Teacher {
         return new Director();
 }
 
+function isDirector(employee: Director | Teacher): boolean {
+    return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (employee instanceof Director)
+        return employee.workDirectorTasks();
+    else
+        return employee.workTeacherTasks();
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === 'Math')
+        return 'Teaching Math';
+    else if (todayClass === 'History')
+        return 'Teaching History';
+}
+
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
+
 console.log(createEmployee('$500'));
+
+const result = executeWork(createEmployee(200))
+console.log(result);
+
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
