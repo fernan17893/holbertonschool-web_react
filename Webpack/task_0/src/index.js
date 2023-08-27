b1970f57537fb9f0bd1ae81eb5b1b57457fe681e
fernan17893 <fernan17893@gmail.com>
2023-08-26 21:23:27 -0400

    big push

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+
+const $ = require("jquery");
+
+$(function() {
+    $(`body`).append(`
+        <p>Holberton Dashboard</p>
+        <p>Dashboard data for students</p>
+        <p>Copyright - Holberton School</p>
+    `);
+});
+
