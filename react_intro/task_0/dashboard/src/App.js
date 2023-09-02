727b3feb723a01f9306240dccf3763ba0a6dd259
fernan17893 <fernan17893@gmail.com>
2023-09-02 19:42:28 -0400

    task 0

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import utils from './utils';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src="./holberton_logo.jpg" className='App-logo' alt='logo' />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+        </body>
+      <footer className='App-footer'>
+        <p>Copyright 2020 - holberton School</p>
+    </footer>
+    </div>
+  );
+}
+
+export default App;
