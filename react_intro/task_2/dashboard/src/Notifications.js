e5060c7bd8c2e9a770d6c8b013c119869caa52ff
fernan17893 <fernan17893@gmail.com>
2023-09-05 11:49:42 -0400

    task 3

--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './Notifications.css';
+import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
 
 
 export default function Notifications() {
     return (
         <div className='Notifications'>
+        <button
+            style={{
+                background: 'none',
+                border: 'none',
+                fontSize: '5px',
+                cursor: 'pointer',
+                position: 'absolute',
+                top: '20px',
+                right: '20px',
+
+            }}
+        aria-label='Close'
+        onClick={(e) => {
+            console.log('Close button has been clicked');
+        }}
+        >
+        <img src={closeIcon} alt='close-icon' />
+        </button>
         <p>Here is the list of notifications</p>
+        <ul>
+				<li data-priority="default">
+					New course available
+				</li>
+				<li data-priority="urgent">
+					New resume available
+				</li>
+				<li
+					data-priority="urgent"
+					dangerouslySetInnerHTML={{__html: getLatestNotification()}}
+				>
+				</li>
+			</ul>
         </div>
     );
-    }
+    };
 
